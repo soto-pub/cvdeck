dac2a7f6dc026f88355b51189716bcbeca5a0851
soto-pub <soto1996.public@gmail.com>
2023-10-22 22:04:10 +0900

    gh-pages用にroutesを改修

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,17 @@ import "./App.css";
 import { Home } from "./pages/Home";
 import { Play } from "./pages/Play";
 import Love from "./pages/Love";
+import { Root } from "./pages/Root";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path={`/`} element={<Home />} />
-          <Route path={`/standard`} element={<Play />} />
-          <Route path={`/love`} element={<Love />} />
+          <Route path={`/`} element={<Root />} />
+          <Route path={`/cvdeck`} element={<Home />} />
+          <Route path={`/cvdeck/standard`} element={<Play />} />
+          <Route path={`/cvdeck/love`} element={<Love />} />
         </Routes>
       </BrowserRouter>
     </div>
